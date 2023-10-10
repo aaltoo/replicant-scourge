31587a760c041302acb2802b3d67faea1e99ec0f
Павлов Станислав Степанович <pavlov_ss@albank.ru>
2023-10-10 12:51:18 +0900

    Fix indentation

--- helpers.py
+++ helpers.py
@@ -1,13 +1,15 @@
 import re
 
+
 def remove_comments(string, lang_extension):
     if lang_extension == ('js' or 'cpp' or 'java' or 'c'):
-      pattern = r"(\".*?\"|\'.*?\')|(/\*.*?\*/|//[^\r\n]*$)" # C-like comment pattern
-      regex = re.compile(pattern, re.MULTILINE|re.DOTALL)
-      def _replacer(match):
-          if match.group(2) is not None:
-              return ""
-          else:
-              return match.group(1)
-      return regex.sub(_replacer, string)
+        pattern = r"(\".*?\"|\'.*?\')|(/\*.*?\*/|//[^\r\n]*$)"  # C-like comment pattern
+        regex = re.compile(pattern, re.MULTILINE | re.DOTALL)
+
+        def _replacer(match):
+            if match.group(2) is not None:
+                return ""
+            else:
+                return match.group(1)
+        return regex.sub(_replacer, string)
 
